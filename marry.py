c2a8ba8596ad673739845a47c4c01d1c510f5e2a
narges-zh <91964286+narges-zh@users.noreply.github.com>
2021-11-17 00:35:30 -0800

    Update marry.py

--- marry.py
+++ marry.py
@@ -7,8 +7,6 @@ girls=['sara','zari','neda','homa','eli','goli','mary','mina']
 marry=[]
 
 for i in range(len(boys)):
-    if len(girls)==0:
-        break
     girl=random.choice(girls)
     boy=random.choice(boys)
     girls.remove(girl)
